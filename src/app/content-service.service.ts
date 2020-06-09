a99a5b7c1a7e493f0496ccb7a815352b68527fc1
HR1Aventum <rohan.arya237@gmail.com>
2020-06-10 02:39:46 +0530

    ~Topics features added

--- src/app/content-service.service.ts
+++ src/app/content-service.service.ts
@@ -8,5 +8,19 @@ export class ContentServiceService {
   topictype : '';
   topicContent: '';
   topicInstructions : '';
+  topics =[] 
   constructor() { }
+  CreateTopic(){
+    let p_topic ={}
+    p_topic['topictitle'] =  this.topictitle
+    p_topic['topictype'] = this.topictype
+    p_topic['topicContent'] = this.topicContent
+    p_topic['topicInstructions'] =this.topicInstructions
+    this.topics.push(p_topic);
+    console.log(this.topics)
+  }
+  deletetopic(index){
+    this.topics.splice(index,1);
+    
+  }
 }
